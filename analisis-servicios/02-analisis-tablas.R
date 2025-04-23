a047eb609b8adb312b3e7a6579ec85fba4ff9a64
Tomás Galassi <tgalassics@gmail.com>
2025-04-23 00:09:43 -0300

    Arreglar títulos de tablas

--- analisis-servicios/02-analisis-tablas.R
+++ analisis-servicios/02-analisis-tablas.R
@@ -25,37 +25,42 @@ tablaPlagasDesague <-
   adorn_pct_formatting(digits = 1)
 
 # Formato gráfico de la tabla
-datatable(tablaPlagasDesague[-1, ], rownames = FALSE, colnames = c("Tipo de desagüe", "Sin plagas", "Con plagas", "Total"))
+datatable(tablaPlagasDesague, rownames = FALSE, colnames = c("Tipo de desagüe", "Sin plagas", "Con plagas", "Total"))
 
 
 # Relación entre tipo de desagüe y la presencia de cucarachas
 tablaCucarachasDesague <-
-  tabyl(datos_limpios, tipo_desague, cucarachas) %>%
+  datos_limpios %>%
+  filter(!is.na(presencia_plagas)) %>%      # Se filtran las observaciones con NA en la variable presencia_plagas
+  tabyl(tipo_desague, cucarachas) %>%
   adorn_totals(where = c("row", "col")) %>%
-  adorn_percentages(denominator = "row") %>%# Distribuciones condicionales
+  adorn_percentages(denominator = "row") %>%
   adorn_pct_formatting(digits = 1)
 
 # Formato gráfico de la tabla
-datatable(tablaCucarachasDesague[-1, ], rownames = FALSE, colnames = c("Tipo de desagüe", "Sin cucarachas", "Con cucarachas", "Total"))
+datatable(tablaCucarachasDesague, rownames = FALSE, colnames = c("Tipo de desagüe", "Sin cucarachas", "Con cucarachas", "Total"))
 
 
 # Relación entre tipo de desagüe y la presencia de mosquitos
 tablaMosquitosDesague <-
-  tabyl(datos_limpios, tipo_desague, mosquitos) %>%
+  datos_limpios %>%
+  filter(!is.na(presencia_plagas)) %>%      # Se filtran las observaciones con NA en la variable presencia_plagas
+  tabyl(tipo_desague, mosquitos) %>%
   adorn_totals(where = c("row", "col")) %>%
-  adorn_percentages(denominator = "row") %>% # Distribuciones condicionales
+  adorn_percentages(denominator = "row") %>%
   adorn_pct_formatting(digits = 1)
 
 # Formato gráfico de la tabla
-datatable(tablaMosquitosDesague[-1, ], rownames = FALSE, colnames = c("Tipo de desagüe", "Sin mosquitos", "Con mosquitos", "Total"))
-
+datatable(tablaMosquitosDesague, rownames = FALSE, colnames = c("Tipo de desagüe", "Sin mosquitos", "Con mosquitos", "Total"))
 
 # Relación entre tipo de desagüe y la presencia de ratas
 tablaRatasDesague <-
-  tabyl(datos_limpios, tipo_desague, ratas) %>%
+  datos_limpios %>%
+  filter(!is.na(presencia_plagas)) %>%      # Se filtran las observaciones con NA en la variable presencia_plagas
+  tabyl(tipo_desague, ratas) %>%
   adorn_totals(where = c("row", "col")) %>%
-  adorn_percentages(denominator = "row") %>% # Distribuciones condicionales
+  adorn_percentages(denominator = "row") %>%
   adorn_pct_formatting(digits = 1)
 
 # Formato gráfico de la tabla
-datatable(tablaRatasDesague[-1, ], rownames = FALSE, colnames = c("Tipo de desagüe", "Sin ratas", "Con ratas", "Total"))
+datatable(tablaRatasDesague, rownames = FALSE, colnames = c("Tipo de desagüe", "Sin ratas", "Con ratas", "Total"))
